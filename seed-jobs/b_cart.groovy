e423828a01d94a63ef8b3558f54645271798dcc7
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-04-07 15:27:40 -0700

    seed job

---
+++ seed-jobs/b_cart.groovy
@@ -0,0 +1,9 @@
+multibranchPipelineJob('CI-Pipelines/cart') {
+    branchSources {
+        github {
+            id('23232323') // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId('GitHub')
+            repository('manojdubey81/cart')
+        }
+    }
+}
